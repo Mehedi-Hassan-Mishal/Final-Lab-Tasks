1fcfb7aa23d2cdac3a12b2f80d6f18bf1a19bb54
Mishal.name <mehedimishal2017@gmail.com>
2021-01-03 23:39:51 +0600

    Quiz2 complete

---
+++ BookSearching/BookSearching/Models/Books.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+using BookSearching.Models;
+
+namespace BookSearching.Models
+{
+    
+    class Books
+    {
+        public int c;
+        
+        public string textbox;
+       public DataTable dtTable = new DataTable();
+        Database objDBaccess = new Database();
+        public void Show()
+        {
+            string query = "SELECT * FROM Books";
+            objDBaccess.readDatathroughAdapter(query,dtTable);
+        }
+        public void Close()
+        {
+            objDBaccess.closeConn();
+        }
+
+        public void performOperation()
+        {
+            string query = "SELECT * fROM Books";
+             c = objDBaccess.executeDataAdapter(dtTable,query);
+            
+        }
+
+        public void searchOpen()
+        {
+
+            string query = "SELECT * FROM Books where Name='"+textbox+"'";
+            objDBaccess.readDatathroughAdapter(query, dtTable);
+        }
+        public void earchClose()
+        {
+            objDBaccess.closeConn();
+        }
+       public void information()
+        {
+            
+        }
+
+
+
+    }
+}
---
+++ BookSearching/BookSearching/Views/Book_Details.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BookSearching.Views
+{
+    public partial class BookDetails : Form
+    {
+        public string name, id, edition, author;
+        public BookDetails()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void BookDetails_Load(object sender, EventArgs e)
+        {
+            NametextBox1.Text = name;
+            IdtextBox2.Text = id;
+            AuthortextBox3.Text = author;
+            EditiontextBox4.Text = edition;
+        }
+    }
+}
